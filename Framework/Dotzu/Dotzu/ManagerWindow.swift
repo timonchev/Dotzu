a0e00431fa317f769397eef2b555f76e92c66f23
Nick Teissler <nick.teissler@affirm.com>
2019-05-15 11:15:47 -0700

    forward shake events

--- Framework/Dotzu/Dotzu/ManagerWindow.swift
+++ Framework/Dotzu/Dotzu/ManagerWindow.swift
@@ -18,8 +18,6 @@ class ManagerWindow: UIWindow {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-
-//        self.backgroundColor = UIColor.clear
         self.windowLevel = UIWindow.Level.statusBar - 1
     }
 
@@ -29,5 +27,22 @@ class ManagerWindow: UIWindow {
 
     override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
         return self.delegate?.isPointEvent(point: point) ?? false
+    }
+
+    /// Affirm override hack: We use a shake motion to trigger our tweaks window: https://github.com/facebook/Tweaks/blob/master/FBTweak/FBTweakShakeWindow.m
+    /// When Dotzu is present, it intercepts these and they terminate at the top of its responder chain.
+    // We need them to be forwarded to other window, notably, our tweak window
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        UIApplication.shared.windows.forEach { window in
+            window.motionBegan(motion, with: event)
+        }
+        super.motionBegan(motion, with: event)
+    }
+
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        UIApplication.shared.windows.forEach { window in
+            window.motionEnded(motion, with: event)
+        }
+        super.motionEnded(motion, with: event)
     }
 }
